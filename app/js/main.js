b6441592f049f1cb4ff3cffbdc16724e0a824a00
Cheh91 <4eh91@ukr.net>
2019-11-26 10:02:06 +0200

    added comments

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    // $(".js-range-slider").ionRangeSlider({
+        
+    //     // type: "double",
+    //     min: 0,
+    //     max: 1000,
+    //     from: 360,
+    //     // removeAttr('style')
+    //     // grid: true
+    //     from_fixed: true,
+        
+        
+        
+        
+    // });
+    // $('.irs-single').attr('style', '');
+    
+    // $(".irs-bar .irs-bar--single").removeAttr("style");
+    
+});
